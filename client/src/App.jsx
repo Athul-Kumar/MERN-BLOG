540323082a60cd91c85205b7db922cd77a4f8fb3
Athul-Kumar <athulkumar7991@gmail.com>
2024-03-19 12:20:58 +0530

    client side structure is created with boilerplate template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = ()=>{
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        MERN BLOG APPLICATION BOILER PLATE TEMPLATE
+      </h1> 
+    </div>
+  );
+}
+
+
+export default App;
