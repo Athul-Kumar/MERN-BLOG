983153cf6153d70d28f40ed0ad533a2cfe752d3a
Athul-Kumar <athulkumar7991@gmail.com>
2024-04-03 19:14:31 +0530

    Mongodb cluster is connected for the backend project
    Problem
    
    Solution
    
    Note

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
-import express from "express"
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.listen(3000,()=>{
-    console.log("the server is running on port 3000!!!!")
-})
+dotenv.config();
+
+const app = express();
+app.listen(3000, () => {
+  console.log("the server is running on port 3000!!!!");
+});
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("successfully connected to mongodb cluster!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
