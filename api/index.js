a87c1a68ff02c3b7479f46bc5fa266422da6ddbf
Athul-Kumar <athulkumar7991@gmail.com>
2024-04-04 11:22:24 +0530

    Created singup api routes
    Problem
    
    Solution
    
    Note

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.router.js"
 
 
 dotenv.config();
 
-
 const app = express();
+
+app.use(express.json())
+
 app.listen(3000, () => {
   console.log("the server is running on port 3000!!!!");
 });
@@ -21,5 +24,5 @@ mongoose
     console.log(err);
   });
 
-
-app.use("/api/user",userRouter);
+app.use("/api/user",userRoutes);
+app.use("/api/auth", authRoutes);
